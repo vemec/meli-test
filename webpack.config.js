303fdaca8874a5e8e4c8b13bb4dc7babe2c720f5
Diego Ghersi <diego@ghersi.com.ar>
2017-07-03 00:27:55 -0300

    Mejoras en la estructura de los folders de public

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, 'public/dist')
     },
     module: {
         rules: [
