e98724cd8b8866fde6171798293605d85089cad3
Diego Ghersi <diego@ghersi.com.ar>
2017-07-04 15:56:10 -0300

    Fix para el 404 + Link en el logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 // Module dependencies
 import React from 'react';
+import { Link } from  'react-router-dom'
 
 // components
 import SearchForm from '../search/SearchForm.jsx';
@@ -13,8 +14,8 @@ class Header extends React.Component {
         <header role="banner" className="nav-header nav-header-ontop">
             <div className="wrap-header">
                 <div className="nav-container">
-                    <a tabIndex="0" className="logo" href="/">Mercado Libre - Test</a>
-                    <SearchForm query={ '' } />
+                    <Link to="/" tabIndex="0" className="logo">Mercado Libre - Test</Link>
+                    <SearchForm />
                 </div>
             </div>
         </header>
--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,7 @@ export default (
         <Layout>
             <Switch>
                 <Route path="/items/:id" component={ (props) => <ItemDetailsContainer /> } />
-                <Route path="/" component={ (props) => <ResultsContainer /> } />
+                <Route exact path="/" component={ (props) => <ResultsContainer /> } />
                 <Route component= { NotFound } />
             </Switch>
         </Layout>
